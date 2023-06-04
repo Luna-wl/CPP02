3441be5df03721c93c339cc4b7bb34c3c4b6537f
Warintorn Luedara <wluedara@z3t7c1.42bangkok.com>
2023-06-04 16:30:42 +0700

    ex01

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_H
+# define FIXED_H
+
+# include <iostream>
+
+class Fixed {
+	private:
+		int _value;
+		static const int _bit = 8;
+	public:
+		Fixed();
+		~Fixed();
+		// copy construct
+		Fixed( const Fixed &num );
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		Fixed &operator=( const Fixed &value );
+};
+
+#endif
